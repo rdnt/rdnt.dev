6b4794951a45bb3a308548881e440fa481966d5b
Tasos Papalyras <tasos@rdnt.dev>
2024-01-11 22:11:42 +0200

    Server-rendered partials, template rendering

--- router/router.go
+++ router/router.go
@@ -1,7 +1,12 @@
 package router
 
 import (
+	"html/template"
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -16,12 +21,54 @@ func New() *Router {
 
 	r.Use(middleware.Logger)
 
-	//r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	//	w.Write([]byte("Hello World!"))
-	//})
+	tpl := template.New("")
+	err := filepath.Walk("web", func(path string, info os.FileInfo, err error) error {
+		if strings.Contains(path, ".gohtml") {
+			_, err = tpl.ParseFiles(path)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+
+		return err
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	type State struct {
+		View string
+	}
+
+	renderApp := func(view string) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			err = tpl.ExecuteTemplate(w, "templates/main", State{View: view})
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+
+	renderPartial := func(partial string) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			err = tpl.ExecuteTemplate(w, partial, nil)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+		}
+	}
+
+	r.Get("/", renderApp("home"))
+	r.Get("/blog", renderApp("blog"))
+	r.Get("/projects", renderApp("projects"))
+
+	r.Get("/partials/home", renderPartial("views/home"))
+	r.Get("/partials/blog", renderPartial("views/blog"))
+	r.Get("/partials/projects", renderPartial("views/projects"))
 
 	fs := http.FileServer(http.Dir("web"))
-
 	r.Handle("/*", http.StripPrefix("/", fs))
 
 	return r
